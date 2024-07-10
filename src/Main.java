90f728835d1e0fe2e6f60e99a732bf49a278b475
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-07-10 13:07:50 +0300

    Ex1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Example 1");
+        int savings = 0;
+        int month = 0;
+        while(savings < 2459000){
+            month ++;
+            savings = savings + 15000;
+            System.out.println("Месяц " + month + ", сумма накоплений равна: " + savings + " рублей.");
+        }
     }
 }
